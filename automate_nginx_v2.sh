0f7e3fb92845c1bc4cccfe7f98f9e5edaf6c5712
Hamza Ali <97181494+Debitsme@users.noreply.github.com>
2024-05-10 16:55:36 +0500

    Update automate_nginx_v2.sh

--- automate_nginx_v2.sh
+++ automate_nginx_v2.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 apt-get install -y nginx
-sudo rm -rf /var/www/html/*
+#sudo rm -rf /var/www/html/*
 echo "Hello World from updated host" $HOSTNAME "!" | sudo tee /var/www/html/index.html
-sudo systemctl restart nginx
+#sudo systemctl restart nginx
